61014ba12b9d1fdc6f090a0ed2c026affd01564b
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-08-18 22:38:24 +0530

    Update adminLogin.php

--- adminLogin.php
+++ adminLogin.php
@@ -151,20 +151,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </div>
 </div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php include "partials/footer.php";  ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
