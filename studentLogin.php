e1d7c89a109bb3b512c5ecdc2f14cc5630947de8
abhijitmondal121 <mondalavi9836@gmail.com>
2023-07-17 21:19:32 +0530

    push

--- studentLogin.php
+++ studentLogin.php
@@ -26,7 +26,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $_SESSION['class'] = $class;
        
         header("location:student_edit.php");
-          
+        
+        
     } 
     else{
       // header("location:studentLogin.php");
